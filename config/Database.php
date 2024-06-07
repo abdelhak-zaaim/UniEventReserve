7a74a846a48ebba07cd7189c8c8505ee26094066
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-06-07 15:36:49 +0100

    implements the database class , and add getCon function

--- config/Database.php
+++ config/Database.php
@@ -1,1 +1,22 @@
 <?php
+
+class Database
+{
+    private $host = "localhost";
+    private $dbname = "reservation";
+    private $user = "root";
+    private $password = "";
+
+    public $cn;
+
+    public function getCon()
+    {
+        $this->cn = null;
+        try {
+            $this->cn = new PDO("mysql:host=$this->host,dbname=$this->dbname", $this->user, $this->password);
+        } catch (PDOException $e) {
+            echo "Connection Error: " . $e->getMessage();
+        }
+        return $this->cn;
+    }
+}
