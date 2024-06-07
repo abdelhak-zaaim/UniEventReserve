a1705f6bb9e2ebc2ddae8d0c7b8f2351fcb64a66
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-06-07 16:01:21 +0100

    implements the render function

--- controllers/ReservationController.php
+++ controllers/ReservationController.php
@@ -3,18 +3,18 @@
 class ReservationController
 {
 
-function render($view, array $data)
-{
-    $viewPath = "/views/$view";
+    function render($view, array $data)
+    {
+        $viewPath = "/views/$view";
 
-    if (!file_exists($viewPath)) {
-        throw new Exception("View file does not exist");
+        if (!file_exists($viewPath)) {
+            throw new Exception("View file does not exist");
+        }
+
+        extract($data);
+        ob_start();
+        require $viewPath;
+        echo ob_get_clean();
     }
-
-    extract($data);
-    ob_start();
-    require $viewPath;
-    echo ob_get_clean();
-}
 
 }
